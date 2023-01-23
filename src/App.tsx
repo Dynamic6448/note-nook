184a24121b1dd0be8357d4ff6fc4b2a3baa9a884
Fawwaz <72813433+Dynamic6448@users.noreply.github.com>
2023-01-23 17:51:41 -0600

    copy paste some login thing

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,17 @@ import { AuthProvider } from './contexts/AuthContext';
 
 const App: React.FC = () => {
     return (
-        // <AuthProvider>
-        //     <Router>
-        //         <Routes>
-        //             <Route path='/' element={<Signin />} />
-        //             <Route path='/login' element={<Signin />} />
-        //             <Route path='/dashboard' element={<Dashboard />} />
-        //         </Routes>
-        //     </Router>
-        // </AuthProvider>
-        <div className='text-9xl font-extrabold text-blue-500'>bruh</div>
+        <AuthProvider>
+            <div className='w-screen h-screen'>
+                <Router>
+                    <Routes>
+                        <Route path='/' element={<Signin />} />
+                        <Route path='/login' element={<Signin />} />
+                        <Route path='/dashboard' element={<Dashboard />} />
+                    </Routes>
+                </Router>
+            </div>
+        </AuthProvider>
     );
 };
 
