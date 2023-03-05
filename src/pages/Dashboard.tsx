6546de49108381a3e3661129a1c06e44b6bbe325
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2023-03-04 18:23:24 -0600

    add dashboard sidebar

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { onValue } from 'firebase/database';
 import { refNotes, deleteNote, setCurrentEditingNoteId, NoteType } from '../firebase';
-import { Card } from '../components/Card';
-import Page from '.';
-import CreateNotePopup from '../components/CreateNotePopup';
-import EditNotePopup from '../components/EditNotePopup';
 import { Button } from '../components/Button';
 import { Note } from '../components/Note';
+import CreateNotePopup from '../components/CreateNotePopup';
+import EditNotePopup from '../components/EditNotePopup';
+import Page from '.';
+import { useAuth } from '../contexts/AuthContext';
 
 const Dashboard: React.FC = () => {
     const [notes, setNotes] = useState<NoteType[]>([]);
@@ -54,33 +54,53 @@ const Dashboard: React.FC = () => {
         setNoteToDelete(-1);
     };
 
+    const { currentUser } = useAuth();
+
     return (
-        <Page>
-            <div className='flex flex-row gap-16 flex-wrap'>
-                {notes.map((note, i) => (
-                    <Note
-                        key={note.id}
-                        title={note.title}
-                        dateCreated={note.dateCreated}
-                        dateUpdated={note.dateUpdated}
-                        deleting={noteToDelete === i}
-                        onTryDelete={() => setNoteToDelete(i)}
-                        onDeleteCancel={() => setNoteToDelete(-1)}
-                        onDeleteConfirm={() => handleDeleteNote(note.id)}
-                        onEdit={() => handleShowEditModal(note.id)}
-                    >
-                        {note.note}
-                    </Note>
-                ))}
+        <div className='w-full h-full flex flex-row'>
+            <div className='w-64 h-full bg-white shadow-xl'>
+                <div className='p-4'>
+                    <p className='text-xl font-semibold'>Dashboard</p>
+                    <p className='text-md'>{currentUser.email}</p>
+                </div>
+                <SidebarButton active>Notes</SidebarButton>
+                <SidebarButton active>Calendar</SidebarButton>
             </div>
+            <Page>
+                <div className='flex flex-row gap-16 flex-wrap'>
+                    {notes.map((note, i) => (
+                        <Note
+                            key={note.id}
+                            title={note.title}
+                            dateCreated={note.dateCreated}
+                            dateUpdated={note.dateUpdated}
+                            deleting={noteToDelete === i}
+                            onTryDelete={() => setNoteToDelete(i)}
+                            onDeleteCancel={() => setNoteToDelete(-1)}
+                            onDeleteConfirm={() => handleDeleteNote(note.id)}
+                            onEdit={() => handleShowEditModal(note.id)}
+                        >
+                            {note.note}
+                        </Note>
+                    ))}
+                </div>
 
-            <Button className='fixed right-6 bottom-6 bg-blue-500 hover:bg-blue-600 text-lg' onClick={handleShowCreateModal}>
-                Create Note
-            </Button>
+                <Button className='fixed right-6 bottom-6 bg-blue-500 hover:bg-blue-600 text-lg' onClick={handleShowCreateModal}>
+                    Create Note
+                </Button>
 
-            <CreateNotePopup show={showCreateNotePopup} handleClose={handleShowCreateModal} />
-            <EditNotePopup show={showEditNotePopup} handleClose={() => handleShowEditModal('')} />
-        </Page>
+                <CreateNotePopup show={showCreateNotePopup} handleClose={handleShowCreateModal} />
+                <EditNotePopup show={showEditNotePopup} handleClose={() => handleShowEditModal('')} />
+            </Page>
+        </div>
+    );
+};
+
+const SidebarButton: React.FC<{ active: boolean; children: React.ReactNode }> = ({ active, children }) => {
+    return (
+        <button className={`w-full py-4 bg-white hover:bg-gray-100 transition text-start`}>
+            <p className='pl-4'>{children}</p>
+        </button>
     );
 };
 
